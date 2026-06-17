877fd5fe6170704aa982e9a6864243eb1573682b
Thiago Braga Costa <thiagobraga.c@gmail.com>
2026-06-16 21:46:19 -0300

    Test: pre-commit hook with type error

--- test-lint-hook.ts
+++ test-lint-hook.ts
@@ -1,9 +1,11 @@
-// Test file with linting errors
+// Test file with linting errors that can't be auto-fixed
 const unusedVariable = 'This should trigger a linting error';
 
 function badFunction() {
   const x = 1;
   console.log('Debug message - should be warned');
+  // This will cause a type error
+  const result: string = 123;
   return x;
 }
 
